99bd5c6bb29ad90d3b0205defe9cd3d0c2088f10
Theodor Vararu <theo@vararu.org>
2015-08-22 13:25:05 +0100

    Show a list of lights.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -9,9 +9,22 @@ App = React.createClass({ // eslint-disable-line
     }
   },
 
+  renderLights () {
+    return this.data.lights.map((light, index) => {
+      return <div key={ light._id }>
+        <strong>Light { index }</strong>
+        <ul>
+          { Object.keys(light.state).map((key) => {
+            return <li key={ light._id + key }>{ key }: { light.state[key].toString() }</li>
+          }) }
+        </ul>
+      </div>
+    })
+  },
+
   render () {
     return <div>
-      <div>{ JSON.stringify(this.data.lights) }</div>
+      <div>{ this.renderLights() }</div>
     </div>
   }
 })
